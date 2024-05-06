ab2a46c31e8ca17e7389c05b01d363c9c77065ca
Zain <zat938@gmail.com>
2024-05-06 17:58:42 +0500

    if applied, this commit adds the vscode settings for working with project

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,19 +1,23 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=19, decimal_places=4)
 
+
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
 
+
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='orders')
     items = models.ManyToManyField(OrderItem)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
+
     def __str__(self) -> str:
         return f'Ordered by user: {self.user.username}'
--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,24 +1,28 @@
 from rest_framework import serializers
-from .models import Item, OrderItem, Order
+
+from .models import Item, Order, OrderItem
+
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ['id', 'title', 'price']
+        fields = ["id", "title", "price"]
+
 
 class OrderItemSerializer(serializers.ModelSerializer):
     item = ItemSerializer()
 
     class Meta:
         model = OrderItem
-        fields = ['id', 'item']
+        fields = ["id", "item"]
+
 
 class OrderSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
     items = OrderItemSerializer(many=True)
-    
-    extra_kwargs = {'user': {'read_only': True}}
+
+    extra_kwargs = {"user": {"read_only": True}}
 
     class Meta:
         model = Order
-        fields = ['id', 'user', 'items', 'start_date', 'ordered_date', 'ordered']
+        fields = ["id", "user", "items", "start_date", "ordered_date", "ordered"]
