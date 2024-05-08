185b49003fdfc83f1fc0e63ef31fd50a8d922401
Zain <zat938@gmail.com>
2024-05-09 02:24:50 +0500

    remove errors

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,7 @@
-import axiosPrivate from "../utils/axios";
-
 const HomePage = () => {
-  const handle = async () => {
-    const response = await axiosPrivate.get("/auth/user");
-    console.log(response);
-  };
   return (
     <div>
       <div>Home Page</div>
-      <button onClick={handle}>Get Items</button>
     </div>
   );
 };
